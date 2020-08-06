e3bc3787792b45d0d698a97016853f5a8fa4c64f
liaofuxing <597429743@qq.com>
2020-08-06 16:01:25 +0800

    fix： 升级 balloon-springboot 到0.1.0

--- src/main/java/com/ts/address/HostAddressApplication.java
+++ src/main/java/com/ts/address/HostAddressApplication.java
@@ -3,7 +3,16 @@ package com.ts.address;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * <p>必须扫描 com.balloon.springboot.core.exception ,否则全局异常拦截不会生效</p>
+ * <p>使用 @ComponentScan 扫描 com.balloon.springboot.core.exception 路径, 是因为该路径下配置了全局异常拦截</p>
+ * <p>要想使用自动装载的 Security, 请在yml中配置开启, 并排除掉 UserDetailsServiceAutoConfiguration.class, 以禁用默认控制台的默认密码</p>
+ *
+ * @author liaofuxing
+ */
+@ComponentScan({"com.balloon.springboot.core.exception", "com.ts"})
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class })
 public class HostAddressApplication {
 
